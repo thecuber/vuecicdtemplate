823d53abc0d292d6535a93817fbca98f35f04878
Thomas VINET <thomas.vinet@inria.fr>
2025-09-06 15:14:35 +0200

    publicpath

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: "/",
+  publicPath: '/vuecicdtemplate/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
